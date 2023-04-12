b9b4ed236dcdfc86246ec521214e442aa2f4f9e4
ajja99 <aja_99@hotmail.com>
2023-04-12 09:48:44 +0200

    init: spring boot aplication

---
+++ WallStreetWarriors/src/test/java/com/g12/wallstreetwarriors/WallStreetWarriorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g12.wallstreetwarriors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallStreetWarriorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
