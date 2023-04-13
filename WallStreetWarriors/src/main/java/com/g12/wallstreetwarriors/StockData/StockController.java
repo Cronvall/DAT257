9404bd7e03d3b2cbd9b48f704d54d3c4987707ba
ajja99 <aja_99@hotmail.com>
2023-04-13 15:07:24 +0200

    Update client to webclient

--- WallStreetWarriors/src/main/java/com/g12/wallstreetwarriors/StockData/StockController.java
+++ WallStreetWarriors/src/main/java/com/g12/wallstreetwarriors/StockData/StockController.java
@@ -1,45 +1,32 @@
 package com.g12.wallstreetwarriors.StockData;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StockController {
-    private static String apiKey = "4933ae9ab60b4f1ca2baf7772887a874";
-    private String symbol;
+
+    private final StockService stockService;
+
+    public StockController(StockService stockService) {
+        this.stockService = stockService;
+    }
 
     record Meta(String symbol, String interval, String currency, String exchange_timezone, String exchange, String mic_code, String type) {}
     record Value(String datetime, String open, String high, String low, String close, String volume) {}
     record StockData(Meta meta, List<Value> values, String status) {}
 
-    private StockData stockData;
-    private List<?> values;
-    private Meta meta;
-    @GetMapping("/stock/{param}")
-    public Meta getStock(@PathVariable String param) {
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        symbol = param;
-
-        String url = String.format("https://api.twelvedata.com/time_series?apikey=%s&interval=1min&symbol=%s&outputsize=1",  apiKey,symbol);
-
-        try {
-            stockData = restTemplate.getForObject(url, StockData.class);
-            values = stockData.values;
-            meta = stockData.meta;
-
-            System.out.println(meta.symbol);
-            System.out.println(values.get(0));
-        } catch (Exception ex) {
-            System.out.println("Error: " + ex.getMessage());
-        }
-        return meta ;
+    @GetMapping("/stock/{ticker}")
+    public ResponseEntity<StockData> getStock(@PathVariable String ticker) {
+        return stockService.getStockByTicker(ticker)
+                .map(data -> new ResponseEntity<>(data, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.OK));
     }
-
-
 }
