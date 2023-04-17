ef38b832a4ff2a192c9de7ec8a8290852015da42
Isac Hansson <isachansson1218@gmail.com>
2023-04-17 14:38:53 +0200

    frontend stuff

--- frontend/app/src/pages/index.tsx
+++ frontend/app/src/pages/index.tsx
@@ -1,13 +1,18 @@
 import { useRouter } from "next/router"
+import React, { useState } from 'react';
+
 
 export default function Home() {
 
   const router = useRouter()
+
   return (
     <>
       <h1>Super mega stock trader league</h1>
       <h2>The leauge for you and your friends</h2>
       <button onClick={() => router.push('/register')}>TEST</button>
+      
+      
     </>
   )
 }
