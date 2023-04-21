71ed539efc5c0adcb7f48423fb1eb898e2a46486
Cronvall <47293083+Cronvall@users.noreply.github.com>
2023-04-21 11:55:25 +0200

    Refactor for holoButtons

--- frontend/app/src/pages/index.tsx
+++ frontend/app/src/pages/index.tsx
@@ -3,8 +3,11 @@ import SignupSection from "../components/signupSection";
 import SampleLeague from "../components/sampleLeague";
 import styles from "../styles/home.module.css";
 import Footer from "../components/footer";
+import { NextRouter, useRouter } from "next/router";
 
 export default function Home() {
+
+  const router: NextRouter = useRouter();
 
   return (
     <>
@@ -12,10 +15,10 @@ export default function Home() {
         <NavBar transparent={true}/>
         <div className={styles.tiltObj} /> 
         <div className={styles.contentContainer}>
-          <SignupSection />
+          <SignupSection router={router}/>
           <SampleLeague />
         </div>
-        <div className={styles.whiteSpace} />
+
       </div>
       <Footer />
     </>
