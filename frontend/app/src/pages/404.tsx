c17ca75825a6f12293ebc74607cc579d979178d0
Cronvall <47293083+Cronvall@users.noreply.github.com>
2023-04-21 11:55:46 +0200

    Update of navbar for 404-page

--- frontend/app/src/pages/404.tsx
+++ frontend/app/src/pages/404.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import navBar from "../components/navBar";
 import styles from "../styles/pageNotFound.module.css";
 import Image from "next/image";
 import ClownImg from "../assets/images/clown.jpeg";
+import NavBar from "../components/navBar";
 
 const NotFoundPage = () => {
     return( 
     <>
-        {navBar()}
+        <NavBar transparent={false}/>
         <h1 className={styles.text}>404 Page not found</h1>
         <Image
             src={ClownImg}
@@ -16,7 +16,7 @@ const NotFoundPage = () => {
             height={150}
             style={{margin: 'auto', display: 'block', marginTop: '100px'}}
         />
-
+        
     </>
     );
 };
