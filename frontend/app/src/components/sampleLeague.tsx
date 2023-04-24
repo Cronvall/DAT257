09432347ee84d47fc8cab9668e0da93467acc0df
Cronvall <47293083+Cronvall@users.noreply.github.com>
2023-04-24 16:23:15 +0200

    table error fix

--- frontend/app/src/components/sampleLeague.tsx
+++ frontend/app/src/components/sampleLeague.tsx
@@ -1,37 +1,30 @@
 import React from "react";
 import styles from "./styles/sampleLeague.module.css";
+import { Table } from "@nextui-org/react";
 
 export default function SampleLeague() {
     return (
         <div className={styles.mainContainer}>
             <div className={styles.leagueContainer}>
-                <table>
-                    <tr className={styles.dataHeader}>
-                        <th>Rank</th>
-                        <th>Username</th>
-                        <th>Gains</th>
-                    </tr>
-                    <tr>
-                        <td>#1</td>
-                        <td>Oscar</td>
-                        <td>+53%</td>
-                    </tr>
-                    <tr>
-                        <td>#2</td>
-                        <td>Keivan</td>
-                        <td>+45%</td>
-                    </tr>
-                    <tr>
-                        <td>#3</td>
-                        <td>Johannes</td>
-                        <td>+42%</td>
-                    </tr>
-                    <tr>
-                        <td>#4</td>
-                        <td>Isac</td>
-                        <td>+40%</td>
-                    </tr>
-                </table>
+                <Table>
+                    <Table.Header>
+                        <Table.Column>RANK</Table.Column>
+                        <Table.Column>USERNAME</Table.Column>
+                        <Table.Column>PROFIT</Table.Column>
+                    </Table.Header>
+                    <Table.Body>
+                        <Table.Row>
+                            <Table.Cell>1</Table.Cell>
+                            <Table.Cell>user1</Table.Cell>
+                            <Table.Cell>$1000</Table.Cell>
+                        </Table.Row>
+                        <Table.Row>
+                            <Table.Cell>2</Table.Cell>
+                            <Table.Cell>user2</Table.Cell>
+                            <Table.Cell>$900</Table.Cell>
+                        </Table.Row>
+                    </Table.Body>
+                </Table>
             </div>
             <div className={styles.textPitch}>
                 <h2>Why Wall St. Warriors?</h2>
