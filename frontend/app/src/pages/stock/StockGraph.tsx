672c812dfdf2a4819d6a9fdd3082f62b92a81bab
Isac Hansson <isachansson1218@gmail.com>
2023-04-26 10:40:41 +0200

    Added a graph to the ui

---
+++ frontend/app/src/pages/stock/StockGraph.tsx
@@ -0,0 +1,36 @@
+import React, {useEffect, useRef, useState} from 'react';
+import { NextPage } from 'next';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import styles from './stock.module.css';
+interface stockgraph {
+  prices: Array<number>;
+dates: Array<string>;
+}
+
+
+
+
+const StockGraph: NextPage<stockgraph> = ({ prices,dates }) => {
+  const data = dates.map((date, i) => ({ date, price: prices[i] }));
+
+  
+  
+
+
+  return (
+    
+      <LineChart width={525} height={275} data={data} className={styles.chart}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="date" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="price" stroke="#8884d8" dot={false} />
+    </LineChart>
+    
+    
+  );
+};
+
+export default StockGraph;
+
