3c0275ccdebdb1c2f9f49f1966c7d071e04cce82
Isac Hansson <isachansson1218@gmail.com>
2023-05-14 18:23:45 +0200

    Functionality to buy stocks, TODO: style

--- frontend/app/src/components/BuyStock.tsx
+++ frontend/app/src/components/BuyStock.tsx
@@ -1,10 +1,11 @@
 import style from "./styles/buystock.module.css"
 //s
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { getCurrentUser } from "@/services/auth.service";
 import { NextPage } from "next";
-import Dropdown from 'react-dropdown';
+
+import { Select } from "@chakra-ui/react";
 
 
 interface IBuyData {
@@ -12,9 +13,19 @@ interface IBuyData {
     ticker: string;
     price: number;
   }
+  interface buyRequest{
+    symbol: string;
+    price: number;
+    amount?: number;
+  }
+  interface Option {
+    label: string;
+    value: string;
+  }
+
  
 const BuyStock: NextPage<IBuyData> = ({ticker,price,onClose}) => {
-
+  const [mounted, setMounted] = useState(false);
   const [amount, setAmount] = useState<number | undefined>(0);
   const [buyPrice, setBuyPrice] = useState<number | undefined>(0);
   const handleNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -26,46 +37,86 @@ const BuyStock: NextPage<IBuyData> = ({ticker,price,onClose}) => {
     
   };
 
-
-
-  const [portfolios, setPortfolios] = useState<string[]>([]);
-
-  const LookupPortfoilos = async () => {
+  const [portfolios, setPortfolios] = useState<Option[]>([]);
+  const [defValue, setDefValue] = useState("");
+  
+  const LookupPortfolios = async () => {
     try {
       const response = await axios.get(`http://localhost:8080/api/rooms?${getCurrentUser()?.id}`);
-      
-      let newPortfolios: string[] = [];
-      response.data.forEach((room: any) => {
+      const newPortfolios = response.data.reduce((acc: Option[], room: any) => {
         room.members.forEach((port: any) => {
           if (port.id.userId === getCurrentUser()?.id) {
-             const portfolioString = `${room.name}: ${port.portfolio.remainingBudget}$`;
-             newPortfolios.push(portfolioString);
+            const portfolioString: Option = {
+              label: `${room.name}: ${port.portfolio.remainingBudget}$`,
+              value: port.id.roomId
+            };
+            if (!acc.some((option) => option.label === portfolioString.label)) {
+              acc.push(portfolioString);
+            }
           }
-  });
-});
-setPortfolios(newPortfolios);
-
+        });
+        return acc;
+      }, []);
+      setPortfolios(newPortfolios);
     } catch (error) {
       console.log(error);
       throw error;
     }
-    
   };
+  
   useEffect(() => {
-    LookupPortfoilos()
-    
+    LookupPortfolios();
   }, []);
-  console.log(portfolios)
+
    
-    
     
 
-    const onSelect= () =>{
-        console.log("new")
+    const onSelect= (event: React.ChangeEvent<HTMLSelectElement>) =>{
+        setDefValue(event?.target.value);
+        
+        console.log(defValue)
     }
-    const defaultOption = portfolios[0]
    
+    const buyButton = async () => {
+      let transaction :buyRequest = {
+        symbol: ticker,
+        price: price,
+        amount: amount
+      }
       
+      try {
+        axios
+        .put(`http://localhost:8080/api/portfolios/${defValue}/stocks/buy`,transaction )
+        .then((response: AxiosResponse) => {
+          console.log("Stock buy order successful", response.data);
+        })
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+      onClose()
+    };
+
+    const sellButton = async () => {
+      let transaction :buyRequest = {
+        symbol: ticker,
+        price: price,
+        amount: amount
+      }
+      
+      try {
+        axios
+        .put(`http://localhost:8080/api/portfolios/${defValue}/stocks/sell`,transaction )
+        .then((response: AxiosResponse) => {
+          console.log("Stock buy order successful", response.data);
+        })
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+      
+    };
+  
 
   return (
     <div className={style.modelpanel}>
@@ -73,13 +124,29 @@ setPortfolios(newPortfolios);
         <div className={style.centering}>
         <p className={style.ticker}>{ticker}</p>
         <p className={style.price}>{price} $</p>
-        <Dropdown className={style.select} options={portfolios} onChange={onSelect} value={defaultOption} placeholder="Select an option" />
+        
 
         <input className={style.amount} type="number" value={amount} onChange={handleNumberChange} placeholder="Amount" />
         <p className={style.cost} > {buyPrice}$</p>
+        <Select
+          className={style.select}
+          placeholder="Select an option"
+          value={defValue}
+          onChange={onSelect}
+          >
+          {portfolios.map((option) => (
+            <option key={option.value} value={option.value}>
+            {option.label}
+            </option>
+          ))}
+          </Select>
+        <button onClick={buyButton} className={style.buybutton}>Buy</button>
+        <button onClick={sellButton} className={style.sellbutton}>Sell</button>
         </div>
         <button onClick={onClose} className={style.button}>Close</button>
+
       </div>
+
       
     </div>
   );
